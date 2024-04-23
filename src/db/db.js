aff72483b1632e35dd1022ebf6bd1a26cc297431
Manjinder-Singh-Bangar <manusharpshooter@gmail.com>
2024-04-23 16:27:10 -0400

    connecting with data base

---
+++ src/db/db.js
@@ -0,0 +1,14 @@
+import { DB_NAME } from "../constants.js";
+import mongoose from "mongoose";
+
+const dbConnect = async ()=>{
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log("MongoDb connected !! Mongodb host:"+ connectionInstance.connection.host)
+    } catch (error) {
+        console.error("Error occured while connecting to the database"+error)
+    }
+}
+
+
+export default dbConnect;
