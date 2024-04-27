bd410cd64cf66d70a35dc4dd435ae0f9984d7f51
Manjinder-Singh-Bangar <manusharpshooter@gmail.com>
2024-04-27 13:17:42 -0400

    all setup configured

--- src/app.js
+++ src/app.js
@@ -11,4 +11,17 @@ app.use(cors({
 
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true, limit:"16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+
+
+// routes
+
+import userRouter from "./routes/user.routes.js";
+
+
+// routes decalaration
+app.use("/api/v1/users", userRouter)
+
 export {app};
