4bcd8b5841688ee74680cee8ad83266ee42be68c
Manjinder-Singh-Bangar <manusharpshooter@gmail.com>
2024-05-02 14:12:30 -0400

    add subscription model

---
+++ src/models/subscription.model.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const subscriptionSchema = mongoose.Schema({
+    subscriber:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+
+    channel:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }
+},{timestamps:true})
